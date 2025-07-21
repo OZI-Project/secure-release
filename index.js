904f2d5a8938fd450cc8e51c15a0f7bb4c0cd23c
Eden Ross Duff MSc <rjdbcm@outlook.com>
2025-07-20 20:45:56 -0500

    :hammer: add debug argument
    Signed-off-by: Eden Ross Duff MSc <rjdbcm@outlook.com>

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
-
+const sdist = core.getBooleanInput('sdist');
+const debug = core.getBooleanInput('debug');
+const oziInternal = core.getBooleanInput('ozi-internal');
 if (core.getInput('wheel-sign-token')) {
   core.setSecret(core.getInput('wheel-sign-token'));
+  if (debug) core.info('wheel-sign-token will be ignored because user set debug.');
 }
 const build = async function(){
   var args = [
     '-m',
     'invoke',
-    `--search-root=.tox/invoke/tmp/${core.getBooleanInput('ozi-internal') ? 'ozi' : 'subprojects/ozi/ozi'}`,
+    `--search-root=.tox/invoke/tmp/${oziInternal ? 'ozi' : 'subprojects/ozi/ozi'}`,
     'release',
-    core.getBooleanInput('sdist') ? ['--sdist'] : [],
-    core.getInput('wheel-sign-token') ? `--wheel-sign-token=${core.getInput('wheel-sign-token')}` : [],
+    sdist ? ['--sdist'] : [],
+    core.getInput('wheel-sign-token') & !debug ? `--wheel-sign-token=${core.getInput('wheel-sign-token')}` : [],
   ];
   try {
-    await exec('python', args.flat(), {silent: true});
+    await exec('python', args.flat(), {silent: !debug});
   } catch (error) {
     core.setFailed(error.message);
   }
